45d47e2e1767377691d08d4c0fdb4101a27f26af
Joshua Yee <joshyee16@gmail.com>
2024-02-06 09:29:39 -0800

    add basic authentication, update header

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,17 @@
+import { Signup } from "./Signup";
+import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
+import { Routes, Route } from "react-router-dom";
+
 export function Content() {
   return (
     <div>
       <h1>Welcome to Cinesthesia!!!</h1>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<LogoutLink />}/>
+      </Routes>
     </div>
   )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <header>
-      <a href="/">Home</a> || <a href="/login">Login</a>
+      <a href="/">Home</a> || <a href="/signup">Create Account</a> || <a href="/login">Login</a> || <a href="/logout">Log Out</a>
     </header>
   )
 }
