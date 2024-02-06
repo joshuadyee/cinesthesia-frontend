427db4f9068ae2b3b94c784ff9969459c9cbe546
Joshua Yee <joshyee16@gmail.com>
2024-02-06 10:55:56 -0800

    add UsersIndex (basic

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,9 +1,25 @@
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { LogoutLink } from "./LogoutLink";
+import { UsersIndex } from "./UsersIndex";
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export function Content() {
+
+  const [users, setUsers] = useState([])
+
+  const handleUsersIndex = () => {
+    console.log("handle User index")
+    axios.get("http://localhost:3000/users.json").then(response => {
+      console.log(response.data)
+      setUsers(response.data)
+    })
+  }
+
+  useEffect(handleUsersIndex, [])
+
   return (
     <div>
       <h1>Welcome to Cinesthesia!!!</h1>
@@ -11,6 +27,7 @@ export function Content() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<LogoutLink />}/>
+        <Route path="/users/index" element={<UsersIndex users={users} />}/>
       </Routes>
     </div>
   )
---
+++ src/UsersIndex.jsx
@@ -0,0 +1,26 @@
+export function UsersIndex(props) {
+  return (
+    <div>
+    <h1>Users Index</h1>
+    {props.users.map(user => (
+      <div key={user.id}>
+        {/* <img src={user.profile_pic}/> */}
+        <p>Email: {user.email}</p>
+        <p>Username: {user.username}</p>
+        <p>Bio: {user.bio}</p>
+        <p>Favorites: </p>
+          <ul>
+            {Object.values(user.films).map(film => (
+              <div>
+                <li key={film.id}>{film.title}</li>
+                <img width="100px" src={film.film_poster}/>
+              </div>
+            ))}
+          </ul>
+        {/* <p>Favorites: {JSON.stringify(user.films)}</p> */}
+        <hr />
+      </div>
+    ))}
+    </div>
+  )
+}
