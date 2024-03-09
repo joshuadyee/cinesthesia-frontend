bdd9c1e0d8b9acfff9419cf189f3cbba7b197414
Joshua Yee <joshyee16@gmail.com>
2024-03-08 16:49:03 -0800

    added conditional header rendering

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,36 +1,33 @@
 import { LogoutLink } from "./LogoutLink"
 
 export function Header() {
-  // let loggedInStatus
 
-  // if (localStorage.jwt) {
-  //   loggedInStatus = (
-  //     <>
-  //       <li><LogoutLink /></li>
-  //     </>
-  //   )
-  // } else {
-  //   loggedInStatus = (
-  //     <>
-  //     <li><a href="/signup">Create Account</a> </li>
-  //     <li><a href="/login">Login</a></li>
-  //     </>
-  //   )
-  // }
+  let loggedInStatus
+
+  if (localStorage.jwt) {
+    loggedInStatus = (
+      <>
+        <a href="/users">Members</a> ||
+        <a href="/films">Films</a> ||
+        <a href="/actors">Actors</a> || 
+        <a href="/directors">Directors</a> ||
+        <a href="/genres">Genres</a> ||
+        <a href="/reviews">Reviews</a> ||
+        <a href="/logout">Log Out</a>
+      </>
+    )
+  } else {
+      loggedInStatus = (
+        <>
+          <a href="/login">Login</a> ||
+          <a href="/signup">Create Account</a> 
+        </>
+      )
+  }
 
   return (
     <header>
-      <a href="/">Home</a> ||
-      <a href="login">Login</a> ||
-      <a href="signup">Create Account</a> ||
-      <a href="/logout">Log Out</a> ||
-      <a href="/users">Users</a> ||
-      <a href="/films">Films</a> ||
-      <a href="/actors">Actors</a> || 
-      <a href="/directors">Directors</a> ||
-      <a href="/genres">Genres</a> ||
-      <a href="/reviews">Reviews</a> ||
-      <a href="/reviews/new">New Review</a> ||
+      <a href="/">Home</a> || {loggedInStatus}
     </header>
   )
 }
--- src/UserReviewsNew.jsx
+++ src/UserReviewsNew.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 export function UserReviewsNew(props) {
   
@@ -11,34 +12,46 @@ export function UserReviewsNew(props) {
     window.location.href = `/reviews`
   }
   
-  return (
-    <div>
-      <h1>New Review</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          {/* Film ID: <input name="film_id" type="number" /> */}
-          <input
-            type="hidden"
-            name="film_id"
-            value={props.film.id || ""}/>
-        </div>
-        <div>
-          Rating: <input 
-            name="rating"
-            type="number"
-            step="0.1"
-            min="0"
-            max="10" />
-        </div> 
-        <div>
-          Review: <input
-            name="review"
-            type="text"
-            value={review}
-            onChange={(e) => setReview(e.target.value)} />
-        </div> <br />
-        <button type="submit">Create Review</button>
-      </form>
-    </div>
-  )
+  if (!localStorage.jwt) {
+    return (
+      <div>
+        <h3>
+          <Link to="/login">
+            Sign in to log, rate or review
+          </Link>
+        </h3>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>New Review</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            {/* Film ID: <input name="film_id" type="number" /> */}
+            <input
+              type="hidden"
+              name="film_id"
+              value={props.film.id || ""}/>
+          </div>
+          <div>
+            Rating: <input 
+              name="rating"
+              type="number"
+              step="0.1"
+              min="0"
+              max="10" />
+          </div> 
+          <div>
+            Review: <input
+              name="review"
+              type="text"
+              value={review}
+              onChange={(e) => setReview(e.target.value)} />
+          </div> <br />
+          <button type="submit">Create Review</button>
+        </form>
+      </div>
+    )
+  }
 }
