fb5b664ccee13de558d2f648d06bd802e0c35a2a
Joshua Yee <joshyee16@gmail.com>
2024-07-25 18:23:22 -0700

    Add styling to Link Bar, completely restyle UserProfile (still need modal for addFavorite)

--- src/UserLinkBar.jsx
+++ src/UserLinkBar.jsx
@@ -1,12 +1,18 @@
 export function UserLinkBar() {
   return (
-    <nav className="flex justify-around p-3 border-1 text-white">
-      <ul className="flex">
-        <li className="mr-3"> 
-          <a className="text-white no-underline inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500" href="/userfilms">Films</a>
+    <nav className="flex p-2 border-1 border-gray-400 bg-gray-700 rounded-md mb-4 ">
+      <ul className="flex  justify-around items-center w-full my-auto">
+        <li > 
+          <a className="text-white no-underline inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" href="/userfilms">Films</a>
         </li>
-        <li className="mr-3"> 
-          <a className="text-white no-underline inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500" href="/userreviews">Reviews</a>
+        <li > 
+          <a className="text-white no-underline inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" href="/userreviews">Reviews</a>
+        </li>
+        <li > 
+          <a className="text-white no-underline inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" href="/">Watchlist</a>
+        </li>
+        <li > 
+          <a className="text-white no-underline inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300" href="/">Likes</a>
         </li>
       </ul>
     </nav>)
--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 import { Link } from "react-router-dom"
 import { UserLinkBar } from "./UserLinkBar"
+import { Modal } from "./Modal"
 
 export function UserProfile({currentUser, films}) {
   const [user, setUser] = useState({films: [], film_users: []})
+  const [isFavoritesModalShowVisible, setIsFavoritesModalShowVisible] = useState(false)
   
+  const favorites = user.films
+
   const getCurrentUser = () => {
     axios.get(`http://localhost:3000/users/${currentUser.user_id}.json`).then(response => {
       console.log("current User data", response.data)
@@ -49,31 +53,55 @@ export function UserProfile({currentUser, films}) {
     })
   }
 
+  const handleShowFavoritesModal = () => {
+    console.log("faves")
+    setIsFavoritesModalShowVisible(true)
+  }
+
+  const handleClose = () => {
+    setIsFavoritesModalShowVisible(false)
+  }
+  console.log("FILMS", films)
+  console.log("FAVORITES", user.films)
+  console.log("REVIEWS", user.film_users)
+
   useEffect(getCurrentUser, [])
 
   return (
-    <div>
-    <h1 className="text-center">{user.username}'s Profile</h1>
-      <img src={user.profile_picture} width="400px" />
+  <div className="p-4">
+    <section className="mt-2">
+      <div className="flex mb-4">
+        <img src={user.profile_picture}  className="h-36 w-36 rounded-full mx-4"/>
+        <div className="flex flex-col">
+          <h1 className="font-medium ">{user.username}</h1>
+          <hr className="border-3 "/>
+          <p className="text-bottom">{user.bio}</p>
+        </div>
+      </div>
       <UserLinkBar />
-      <p>{user.bio}</p>
-      <p>Email: {user.email}</p>
-       <h2>Favorites</h2>
-        <ul>
-          {user.films.map(film => (
-            <div key={film.id}>
-              <li>
-                <Link to={`/films/${film.id}`}>
-                  <img width="100px" src={film.film_poster} 
-                /></Link>
-              </li>
-              <p>
-                <button onClick={() => removeFavorite(film)}>Remove From Favorites</button>
-              </p>
-            </div>
-          ))}
-        </ul>
-      <form onSubmit={addFavorite}>
+    </section>
+    <div className="mb-4">
+      <h2 className="uppercase text-3xl">Favorite Films</h2>
+      <hr className="border-1"/>
+      <ul className="flex flex-row gap-5">
+        {user.films.map(film => (
+          <div key={film.id}>
+            <li className="group">
+              <a href={`/films/${film.id}`}>
+                <img src={film.film_poster} className="w-40 h-full rounded-lg object-contain group-hover:border-2 group-hover:border-blue-500" alt={film.title}
+              />
+              </a>
+            </li>
+            <p>
+              <button onClick={() => removeFavorite(film)}>Remove From Favorites</button>
+            </p>
+          </div>
+        ))}
+      </ul>
+      <button className="py-1 px-1 border-1 bg-blue-500 rounded-full" onClick={handleShowFavoritesModal}>
+        Edit Favorites
+      </button>
+      {/* <form onSubmit={addFavorite}>
         <label>
           Add a film to Favorites
           <select name="film_id" id="film">
@@ -85,17 +113,36 @@ export function UserProfile({currentUser, films}) {
           </select>
         </label>
         <button type="submit">Add to Favorites</button>  
-      </form>
-      <h2>Reviews by {user.username}</h2>
-        <ul>
-          {user.film_users.map(review => (
-            <div key={review.id}>
-              <li><h3>{review.film}</h3></li>
-                <p>Rating: {review.rating}</p>
-                <p>Review: {review.review}</p>
-            </div>
-          ))}
-        </ul>
+      </form> */}
+
+    </div>
+    <div>
+      <h2 className="uppercase text-2xl">Recently Watched Films</h2>
+      <hr className="border-1"/>
+      <ul className="flex flex-row gap-5 ">
+      {user.film_users
+        .slice(0,5)
+        .filter(film_user => film_user.watched)
+        .map(film_user => (
+            <li className="group">
+              <a href={`/films/${film_user.film_id}`}>
+                <img src={film_user.film_poster} alt="film_poster" className="w-40 h-full rounded-lg object-contain group-hover:border group-hover:border-blue-500"/>
+              </a>
+            </li>
+        ))
+      }
+      </ul>
+    </div>
+    {/* <h2>Reviews by {user.username}</h2>
+      <ul>
+        {user.film_users.map(review => (
+          <div key={review.id}>
+            <li><h3>{review.film}</h3></li>
+              <p>Rating: {review.rating}</p>
+              <p>Review: {review.review}</p>
+          </div>
+        ))}
+      </ul> */}
     <footer>Account created on {user.created_at}</footer>
   </div>
   )
