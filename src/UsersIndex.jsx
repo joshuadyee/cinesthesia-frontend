a66d5e881368fd36d2fe35c7b72198dd7a17f4cd
Joshua Yee <joshyee16@gmail.com>
2024-07-30 18:30:39 -0700

    Restyle UsersIndex (need to add icons for and refactor links to watched, reviews, and likes)

--- src/UsersIndex.jsx
+++ src/UsersIndex.jsx
@@ -14,41 +14,67 @@ export function UsersIndex() {
     })
   }
 
+  const reviewCount = (filmUsers) => {
+    return filmUsers.filter(filmUser => filmUser.review !== null).length
+  }
+
   useEffect(handleUsersIndex, [])
   
   return (
-    <section className="p-5">
-      <h1>Cinesthesia Members</h1>
-      <form className="pb-4 text-right">
-        <input 
-          placeholder="Search by Username..."
-          type="text"
-          value={searchFilter}
-          onChange={event => setSearchFilter(event.target.value)}
-          className="text-black bg-purple-white shadow rounded border-0 p-2"
-        />
-      </form>
+    <section className="p-4">
+      <div className="flex justify-between mb-4 ">
+        <h1 className="text-4xl font-semibold ">Fellow Film Fans and Critics</h1>
+        <form>
+          <input 
+            placeholder="Search by Username..."
+            type="text"
+            value={searchFilter}
+            onChange={event => setSearchFilter(event.target.value)}
+            className="text-black bg-purple-white shadow rounded border-0 p-2"
+          />
+        </form>
+      </div>
       {users
         .filter(user => 
           user.username.toLowerCase().includes(searchFilter.toLowerCase()))
         .map(user => (
-      <div key={user.id} className="focus:outline-none">
-        <img src={user.profile_picture} width="150px"/>
-        <Link to={`/users/${user.id}`} className="focus:outline-none">
-          <h3 className="focus:outline-none">{user.username}</h3>
-        </Link>
-        <h3>Recent Reviews by {user.username}</h3>
-          <ul>
-            {user.film_users
-            .slice(0,3)
-            .map((film_user, i) => (
-              <div key={i}>
-                <h5>{film_user.film}</h5>
-                <p>{film_user.review}</p>
-              </div>
-            ))}
-          </ul>
-        <hr />
+      <div key={user.id} className="flex flex-col">
+        <div className="flex mb-2">
+          <img src={user.profile_picture} className="w-14 h-14 object-contain rounded-full border-1 border-white mr-4"/>
+          <h3 className="text-3xl content-center ">
+            <a href={`/users/${user.id}`} className=" text-white no-underline hover:underline">
+              {user.username}
+            </a>
+          </h3>
+          <div className="w-full flex justify-evenly ">
+            <a href={`/userfilms`} className="content-center no-underline text-inherit hover:text-blue-500">
+              Films watched: {user.film_users.length}
+            </a>
+            <a href={`/userreviews`} className="content-center no-underline text-inherit hover:text-blue-500">
+              Reviews: {reviewCount(user.film_users)}
+            </a>
+            <a href="/" className="content-center no-underline text-inherit hover:text-blue-500">Likes: </a>
+          </div>
+        </div>
+        <div className="text-neutral-200">
+          <h3 className="text-2xl">Recent Activity</h3>
+            <ul >
+              {user.film_users
+              .slice(0,2)
+              .map((film_user, i) => (
+                <div key={i}>
+                  <h5 className="flex">
+                    <img src={film_user.film_poster} alt="film_poster" className="w-12 h-12 object-contain rounded-2xl shadow-md mr-2" />
+                    <a href={`/films/${film_user.film_id}`} className="no-underline text-inherit hover:text-sky-400 content-center">
+                      {film_user.film}
+                    </a>
+                  </h5>
+                  <p className="indent-4">"{film_user.review}"</p>
+                </div>
+              ))}
+            </ul>
+        </div>
+          <hr />
       </div>
       ))}
     </section>
